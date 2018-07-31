a718a350e92b79b18623c6e8c83e2b514d29e8ae
Nzingani <isabelnzingani@outlook.com>
2018-07-31 15:01:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>P6ex2</title>
+  </head>
+  <body>
+    <?php
+        if (isset($_GET['week']))//on verifie le nombre de semaine
+        {
+            echo $_GET['week'];
+        }
+        ?>
+  </body>
+</html>
